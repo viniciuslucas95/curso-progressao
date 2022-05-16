8b7ebc5ad19c53ca241e7ce484d6ca230f939745
Vinícius Lucas <viniciuslucas1564@gmail.com>
2022-05-16 13:53:33 -0300

    Added initial tests

---
+++ CursoProgressao.Server.Test/StringExtentionTests.cs
@@ -0,0 +1,27 @@
+﻿using CursoProgressao.Server.Utils;
+using Xunit;
+
+namespace CursoProgressao.Server.Test;
+
+public class StringExtentionTests
+{
+    [Fact]
+    public void ShouldTransformCamelCaseIntoPascalCase()
+    {
+        string expected = "FirstName";
+
+        string actual = "firstName".ToPascalCase();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void ShouldTransformPascalCaseToSentence()
+    {
+        string expected = "First name";
+
+        string actual = "FirstName".ToSentence();
+
+        Assert.Equal(expected, actual);
+    }
+}
